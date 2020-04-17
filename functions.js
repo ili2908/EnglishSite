aa410e5e8b4657b98e0b7c6b0376419ca8d9c320
ili2908 <user.email>
2020-04-17 18:13:06 +0300

    m

--- functions.js
+++ functions.js
@@ -1,15 +1,27 @@
 const getButton=document.getElementById("get");
 const question=document.getElementById("Question");
 const answer=document.getElementById("Answer");
-
+const percent=document.getElementById("percent");
 const database=firebase.database(); 
 const rootRef=database.ref("answers");
 console.log(rootRef);
 getButton.addEventListener("click",(e)=>{
     e.preventDefault();
     rootRef.orderByKey().on('value',snapshot=>{
-        console.log(snapshot.val());
 
-
+        var answers = snapshot.val()[question.value];
+        
+        var cur=-1;
+        var curi;
+        var sum;
+        for(var i in answers){
+            if(answers[i]>cur){
+                cur=answer[i]
+                curi=i;
+            }
+            sum+=answers[i];
+        }
+        answer.value=curi;
+        percent.innerText=(answers[curi]/sum)*100;
     });
 });
