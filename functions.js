38cca2cf6c7371f6d0476a5191e7f91a747087fa
Chalen20 <47479508+Chalen20@users.noreply.github.com>
2020-04-17 19:11:47 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,57 @@
 const getButton=document.getElementById("get");
+const submitButton = document.getElementById("submit");
 const question=document.getElementById("Question");
 const answer=document.getElementById("Answer");
 const percent=document.getElementById("percent");
 const database=firebase.database(); 
 const rootRef=database.ref("answers");
 console.log(rootRef);
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    var number;
+    var answers;
+    rootRef.orderByKey().on('value', snapshot => {
+        answers = snapshot.val()[question.value];
+        console.log(answers);
+        if (!answers) {
+            number = -1;
+        } else if (answer.value in answers) {
+            number = answers[answer.value];
+        } else {
+            number = 0;
+        }
+    });
+    setTimeout(()=> {
+        console.log("number " + number);
+        if (number == -1) {
+            var o = {};
+            o[answer.value] = 1;
+            rootRef.child(question.value).set(o);
+        } else if (number == 0) {
+            var o = {};
+            o[answer.value] = 1;
+            for(var i in answers){
+                o[i] = answers[i];
+            }
+            const newData = o;
+            const updates = {};
+            updates['/answers/' + question.value] = newData;
+            database.ref().update(updates);
+        } else if (number) {
+            var o = {};
+            o[answer.value] = number + 1;
+            for(var i in answers){
+                o[i] = answers[i];
+            }
+            const newData = o;
+            const updates = {};
+            updates['/answers/' + question.value] = newData;
+            database.ref().update(updates);
+        }
+    }, 10);
+});
+
 getButton.addEventListener("click",(e)=>{
     e.preventDefault();
     rootRef.orderByKey().on('value',snapshot=>{
